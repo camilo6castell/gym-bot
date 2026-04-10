c54d8151967bbb546fc9722ae27cf1df9ecbb336
Camilo Castellanos <kml107@hotmail.com>
2026-04-10 08:48:56 -0500

    gpu chromium disabled and stable

--- bot/browser.py
+++ bot/browser.py
@@ -71,12 +71,12 @@ def launch_chromium(headless=False):
             "--disable-blink-features=AutomationControlled",
             "--no-first-run",
             "--no-default-browser-check",
-            # ← flags para deshabilitar GPU
             "--disable-gpu",
             "--disable-gpu-compositing",
             "--disable-gpu-rasterization",
             "--disable-software-rasterizer",
-            "--disable-dev-shm-usage",  # ← ayuda en sistemas con poca memoria compartida
+            "--disable-dev-shm-usage",
+            "--ozone-platform=x11",
         ],
         ignore_default_args=["--enable-automation", "--no-sandbox"],
         no_viewport=True,
